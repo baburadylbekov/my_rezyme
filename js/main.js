f30aab4e0a6cda3d92b86b154678b83d23e2e114
haku <adylbekovhaku@gmail.com>
2021-12-09 20:11:12 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,109 @@
+// !методы мосива 
+// масив-это упорядочная список элементов.для того чтобы облегчить разработчку работу многие рутины задачы для каждого из типов даных были сохранены в стандартоный  библотеке вв иде методов 
+// с масив чаще всего предолевают операци добавления и кдаления элементов имено для этих существуют методы push pop shrift un
+
+
+
+
+// !push/pop
+// для добавления новых элементов в конец масива есть метод push синтаксис котого выглядит так название масива push (новый элемент)
+// let arr = ['harry','ron','draco', 'hermiona', 42,22,10,]
+// console.log(arr{22});
+// console.log(arr[arr.length]);
+// arr.push([13,6])
+// arr.push(null)
+// arr.push(11)
+// arr.push('hello')
+// arr.push(true)
+
+
+
+
+
+
+
+
+
+// !shift/unshift
+// let arr = ['harry','ron','draco', 'hermiona', 42,22,10,]let arr = ['harry','ron','draco', 'hermiona', 42,22,10,]
+// ['hello', 'nice']
+// arr.unshift('makers')
+// arr.unshift(1,2,3,4)
+// arr.shift
+// console.log(arr)
+
+
+
+
+
+//! slice\splice
+// для того что бы работать в произвольном участке масива используется такие методы как spalicei slice
+// методы slice копирует и возврощяет новый масив принимает в аргументы индексы начальную позицую с кого индекса начать и конечную позицию до кого элементы копировать не включеить 
+// название , 'watermelon', pineapple]
+
+
+// let fruits = ['apple', 'banana', 'peach', 'watermelon', 'pineapple']
+// let result = fruits.slice(2, 4)
+// console.log(fruits);
+// console.log(result);
+
+// методы splice боле  функцоналне  тк рон может удалят и заменят элементы  в производстве изначальный масив
+// названия масива splice индекс кол во элементов для удаления элементы котрые нужно вствить 
+
+
+
+
+
+
+
+// let fruits = ['apple', 'banana', 'peach', 'watermelon', 'pineapple']
+// console.log(fruits);
+// let result = fruits.splice(1, 0, 'vsem privet')
+// console.log(result);
+// console.log(fruits);
+
+// join sprlit reverse
+//  с помощю метода joina можнообядинить все элементы масива в одну строку 
+// названия масива join  соидинения 
+// в скобках метод принимает соединение котрый вставляет междлу оделениями  элементами масива 
+// let fruits = ['apple', 'banana', 'peach', 'watermelon', 'pineapple']
+ 
+// let joinnedstring = fruits.join('+')
+// console.log(joinnedstring);
+
+// противополжный метод join является метод  split методы sprilt
+// рабивает элементы сторки в масиве по указоному разделителю
+// название масива splint разделить
+// let str = 'makers first bootcamp in kg'
+// let worbs =str . split ('')
+// console.log(worbs);
+// let date ='2021/10/06'
+// let year=date.split('/')
+// let year=date.split('/')
+// let year=date.split('/')
+// console
+// метод revers переварачивает и менят поорядок элементов в обратную сторану 
+// let arr =['first', 'second', 'thrd']
+// let reversedar= arr.reverse
+// let str = 'hello! my name is tilek';
+// let str2 = str. split('').reverse
+// console.log(str2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
